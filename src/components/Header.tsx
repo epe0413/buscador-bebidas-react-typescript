c62d70fb73c79b50e4fffcff0d575e681bcc09f2
Everth Paredes <epe0413@gmail.com>
2024-06-06 17:11:14 -0600

    Creating the Search Form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { useMemo } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 
 export default function Header() {
 
     const {pathname} = useLocation()
-    console.log(pathname)
+    
+    const isHome = useMemo(() => pathname === '/', [pathname])
 
     return (
         <header className="bg-slate-800">
@@ -27,6 +29,43 @@ export default function Header() {
                         >Favoritos</NavLink>
                     </div>
                 </div>
+                {isHome && (
+                    <form
+                        className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-20 p-10 rounded-lg shadow space-y-6'
+                    >
+                        <div>
+                            <label
+                                htmlFor="ingredient"
+                                className='block text-white font-extrabold text-lg'
+                            >Nombre o Ingredientes</label>
+                            <input
+                                id='ingredient'
+                                name='ingredient'
+                                type="text"
+                                className='p-3 w-full rounded-lg focus:outline-none'
+                                placeholder='Nombre o Ingrediente. Ej. Vodka, Tequila, Cafe'
+                            />
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="category"
+                                className='block text-white font-extrabold text-lg'
+                            >Categoría</label>
+                            <select
+                                id='category'
+                                name='category'
+                                className='p-3 w-full rounded-lg focus:outline-none'
+                            >
+                                <option value="">-- Seleccione --</option>
+                            </select>
+                        </div>
+                        <input
+                            type="submit"
+                            value='Buscar Recetas'
+                            className='cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded'
+                        />
+                    </form>
+                )}
             </div>
         </header>
     )
