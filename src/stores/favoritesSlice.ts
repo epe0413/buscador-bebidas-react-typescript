adb83ce7ab04b9293892fe97ff14e13f157219d2
Everth Paredes <epe0413@gmail.com>
2024-06-10 17:38:19 -0600

    Adding Favorites

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -3,10 +3,22 @@ import { Recipe } from '../types'
 
 export type FavoritesSliceType = {
     favorites: Recipe[]
+    handleClickFavorite: (recipe: Recipe) => void
 }
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType> = () => ({
-    favorites: []
-})
+export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
+    favorites: [],
+    handleClickFavorite: (recipe) => {
+        if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)){
+            set((state) => ({
+                favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink)
+            }))
+        } else {
+            console.log('No existe...')
 
-// Slice Pattern
+            set((state) => ({
+                favorites: [ ...state.favorites, recipe]
+            }))
+        }
+    }
+})
