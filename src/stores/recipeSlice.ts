893bfae5dec2945bc819c4899fb7e06af5004792
Everth Paredes <epe0413@gmail.com>
2024-06-10 10:21:06 -0600

    Querying the API to obtain recipe details by ID

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,5 @@
 import { StateCreator } from "zustand"
-import { getCategories, getRecipes } from "../services/RecipeService"
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
 import type { Categories, Drink, Drinks, SearchFilter } from "../types"
 
 export type RecipiesSliceType = {
@@ -30,6 +30,7 @@ export const createRecipesSlice : StateCreator<RecipiesSliceType> = (set) => ({
         })
     },
     selectRecipe: async(id) => {
-        console.log(id)
+        const selectedRecipe = await getRecipeById(id)
+        console.log(selectedRecipe)
     }
 })
